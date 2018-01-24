5e0f7c4b47d4cc17c20d89cea3525802f5ad0a1a
WarrenTseng <r05631018@ntu.edu.tw>
2018-01-24 21:29:53 +0800

    Update train.py

--- Final_Chat-Bot/train.py
+++ Final_Chat-Bot/train.py
@@ -8,20 +8,21 @@ import jieba
 import os
 import time
 
+model_name = 'test2'
 sent_len = 10
-model_name = 'test'
+
 # new model
-os.system('mkdir ./model/'+model_name)
+_ = os.system('mkdir ./model/'+model_name)
 
 # new word2vec model
 word2vec_lookup = Word2vec(model_name)
-dloader = loader(word2vec_lookup, mode='new', model_name=model_name, sent_len=sent_len)
+dloader = loader(word2vec_lookup, mode='new', model_name=model_name)
 
 # pre-trained word2vec model
 # dloader = loader(mode='pre_trained', model_name=model_name, sent_len=sent_len)
 
 # loading the training data and pre-processing
-dloader.data_loading()
+dloader.data_loading(sent_len=sent_len)
 
 # hyperparameters
 np.set_printoptions(precision=2)
@@ -30,14 +31,15 @@ vec_size = len(dloader.word2vec_lookup['<unk>'])
 oneHot_size = dloader.voca_size
 enc_len = sent_len
 dec_len = enc_len
-n_layer1 = 1024
+n_layer1 = 512
 l_r = 1e-3
-epoch = 3
+epoch = 8
 batch_size = 128
-n_hiddens = 3
+n_hiddens = 2
 rnn_cell = tf.contrib.rnn.BasicLSTMCell
 op = tf.train.AdamOptimizer
-max_gradient_norm = 5
+max_gradient_norm = 1
+
 # seq2seq initializing
 s2s = seq2seq_chatbot(oneHot_size=oneHot_size,
                       l_r=l_r,
@@ -62,6 +64,7 @@ loss_all = s2s.loss_func(word_outputs=word_outputs,
 update_step = s2s.optimizer(op=op,
                             loss_all=loss_all,
                             max_gradient_norm=max_gradient_norm)
+
 # functions for predicting next sentences
 def next_sent(inp):
     inp = list(jieba.cut(inp))
@@ -85,6 +88,11 @@ def next_sent(inp):
         resp.append(sess.run([word_outputs], feed_dict=feed_dict)[0][k])
         batch_y[0][k] = np.argmax(resp[k])
     print(np.array(sess.run(Attention_weights[0:enc_len], feed_dict=feed_dict)).reshape([-1]))
+#     atten_w = np.array(sess.run(Attention_weights, feed_dict=feed_dict)).reshape([-1])
+#     atten_avg = list()
+#     for i in range(enc_len):
+#         atten_avg.append(np.mean([atten_w[i+j*enc_len] for j in range(dec_len)]))
+#     print(np.array(atten_avg))
     sent = [dloader.oneHotPos2word[np.argmax(w)] for w in resp]
     return sent
 def next_n_sent(inp, n):
@@ -94,16 +102,20 @@ def next_n_sent(inp, n):
         sents.append(resp)
         inp = ''.join(sents[i]).replace('<end>', '')
     return sents
+
 # training
 init = tf.global_variables_initializer()
 ls = list()
 with tf.Session() as sess:
     sess.run(init)
+    saver = tf.train.Saver()
     print('Initial state:')
     for epo in range(epoch):
         t_start = time.time()
-        inp = '什麼時候要再過來玩'
-        print(next_sent(inp))
+        inp = '到現在我還是很懷疑'
+        r = next_n_sent(inp, 5)
+        for j in range(len(r)):
+            print(''.join(r[j]))
         l = list()
         for i in range(int(len(dloader.sent_seg)/batch_size)):
             batch_x = dloader.batch_X(i*batch_size, batch_size)
@@ -124,9 +136,11 @@ with tf.Session() as sess:
         ls.append(np.mean(l))
         #-----test-----
         inp = '改天要麻煩你一件事'
-        resp = next_sent(inp)
+        resp = next_n_sent(inp, 5)
         print('Input: '+''.join(inp))
-        print('Output: '+''.join(resp))
+        print('Output: ')
+        for j in range(len(resp)):
+            print(''.join(resp[j]))
         # plot the loss
         plt.plot(np.arange(len(l)), l)
         plt.title('Loss (epoch'+str(epo+1)+')')
@@ -138,8 +152,9 @@ with tf.Session() as sess:
     s2s.save_model(sess=sess,
                    saver=saver,
                    name='test')
+    s2s.save_model_params_meta()
     # plot the loss
-    plt.plot(np.arange(len(ls)), ls)
+    plt.plot(np.arange(len(ls))+1, ls)
     plt.title('Loss')
     plt.xlabel('epoch')
     plt.ylabel('loss')
